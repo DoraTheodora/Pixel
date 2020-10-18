8614c8889fc7c4e62c5748b4b3122ac09710bf35
Theodora <tataru.theodora@yahoo.com>
2020-10-18 16:12:44 +0100

    Servos methods implemented

---
+++ Implementation/servos_together.py
@@ -0,0 +1,25 @@
+## Theodora Tataru
+## Pixel the echodot
+## Servos Script
+
+import pantilthat
+from time import sleep
+import RPi.GPIO as GPIO
+
+def setServoAngle(servo, angle):
+    ## vertical
+    if servo == 'pan':
+        pantilthat.servo_two(angle)
+    ## horizontal
+    if servo == 'tilt':
+        pantilthat.servo_one(angle)
+    sleep(0.3)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        setServoAngle('pan', 0)
+        setServoAngle('tilt', 0)
+    else:
+        setServoAngle('pan', int(sys.argv[1]))
+        setServoAngle('tilt', int(sys.argv[2]))
